19cbd97f40573d3f9b80529e23b7d11e8fb45626
raniyaptla <ra.niyapoonthala@gmail.com>
2024-01-18 15:03:46 +0530

    upt

---
+++ app.js
@@ -0,0 +1,16 @@
+
+
+// // Progression 1:
+const container = document.getElementById('react-container');
+ReactDOM.render("Hello! Welcome to React",container);
+
+// // // Progression 2:
+
+// const Container = () =>{
+//   return React.createElement(`div`,null,`Hey Kalvians! Welcome to React Learning`,
+//      React.createElement(`div`,null,`Let's rock and roll`)
+//      );
+// }
+
+// const containerTwo = document.getElementById('react-container');
+// ReactDOM.render(React.createElement(Container),containerTwo);
